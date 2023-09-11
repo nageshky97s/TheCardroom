30a55d099bf6c50bcca3e60838a9ae1c58fee358
unknown <nageshky97s@gmail.com>
2023-09-11 19:00:59 +0530

    Freetype long to unsigned int issue

--- Header Files/FreetypeRender.h
+++ Header Files/FreetypeRender.h
@@ -22,7 +22,8 @@ class FreetypeRender
 			unsigned int TextureID;
 			glm::ivec2 Size;
 			glm::ivec2 Bearing;
-			unsigned int Advance;
+			//unsigned int Advance;
+			long Advance;
 		};
 		unsigned int VAO, VBO;
 		std::unordered_map<char, Character> Characters;
